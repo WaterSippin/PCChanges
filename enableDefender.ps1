29f5cb2d8af426ce2facfff225668e743837c886
WaterSippin <103482683+WaterSippin@users.noreply.github.com>
2024-02-16 10:32:59 -0800

    Add files via upload

---
+++ enableDefender.ps1
@@ -0,0 +1,13 @@
+# Set execution policy
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# Get the directory of the script
+$ScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+
+# Construct the full path to the optimizer.exe
+$OptimizerPath = Join-Path $ScriptPath "..\Programs\optimizer.exe"
+
+
+Start-Process -FilePath $OptimizerPath -ArgumentList "/restart=EnableDefender"
+
