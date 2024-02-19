245631a722f79a8c7aaba01aa4ba4d8136c36fed
WaterSippin <103482683+WaterSippin@users.noreply.github.com>
2024-02-18 17:05:05 -0800

    Update RunOptimzer.ps1

--- RunOptimzer.ps1
+++ RunOptimzer.ps1
@@ -1,6 +1,11 @@
--ExecutionPolicy Bypass
-# set working directory to Desktop/Files/Applications
-Set-Location "$env:USERPROFILE\Desktop\Files\applications"
-Configfile = "$env:USERPROFILE\Desktop\Files\Scripts\template10.json"
-# run the optimizer
-optimizer.exe /config=$Configfile
+# Set execution policy
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+# Set working directory to Desktop/Files/Applications
+Set-Location "$env:USERPROFILE\Desktop\Files\Applications"
+
+# Define the path to the configuration file
+$Configfile = "$env:USERPROFILE\Desktop\Files\Scripts\template10.json"
+
+# Run the optimizer
+.\optimizer.exe /config=$Configfile
