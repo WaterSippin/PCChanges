13ea8401964936a76a8896e0fdea1c42a3dfb112
WaterSippin <103482683+WaterSippin@users.noreply.github.com>
2024-02-20 10:37:35 -0800

    Update RunOptimzer.ps1

--- RunOptimzer.ps1
+++ RunOptimzer.ps1
@@ -4,8 +4,18 @@ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 # Set working directory to Desktop/Files/Applications
 Set-Location "$env:USERPROFILE\Desktop\Files\Applications"
 
-# Define the path to the configuration file
+# Initial default configuration file assuming Windows 10
 $Configfile = "$env:USERPROFILE\Desktop\Files\Scripts\template10.json"
+
+# Check if running on Windows 11 by examining the build number
+# Windows 11 starts from build number 22000
+if ((Get-ComputerInfo).WindowsBuildLabEx -match '(\d+)' -and $matches[1] -ge 22000) {
+    # Define the path to the Windows 11 configuration file
+    $Configfile = "$env:USERPROFILE\Desktop\Files\Scripts\template11.json"
+}
+
+# Display which config file is being used (optional)
+Write-Host "Using configuration file: $Configfile"
 
 # Run the optimizer
 .\optimizer.exe /config=$Configfile
